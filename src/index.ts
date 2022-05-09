ed6c12d93ec14a8091b128d6210165bd5ab9b2eb
Jeff <chienj.22@gmail.com>
2022-05-09 23:24:04 +0200

    Project setup, add ignore files, linters, packages and types. Project entry point index.js

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express = require('express');
+import dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
